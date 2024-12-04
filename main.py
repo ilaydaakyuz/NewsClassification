2b7c830fc61caa8db4c577bc90e35435c3f6bf8b
Sumeyye Karakus <sumeyyekarakusk@gmail.com>
2024-12-04 15:27:01 +0300

    The modification has been made.

--- main.py
+++ main.py
@@ -4,16 +4,17 @@ import os
 
 def main():
     # JSON dosyasının doğru yolunu tanımlıyoruz
-    dataset_path = os.path.join('data', 'News_Category_Dataset_v3.json')  # Dosya adı gereksinime göre düzenlenebilir
+    dataset = os.path.join('data', 'News_Category_Dataset_v3.json')  
     
     # JSON dosyasını açma
-    with open(dataset_path, 'r', encoding='utf-8') as f:
+    with open(dataset, 'r', encoding='utf-8') as f:
         data = [json.loads(line) for line in f]
 
     # Veriyi DataFrame'e dönüştürme
     df = pd.DataFrame(data)
     
     # İlk 5 satırı kontrol etmek için yazdırıyoruz
+    #daha sonra silinebilir
     print(df.head())
 
 if __name__ == "__main__":
