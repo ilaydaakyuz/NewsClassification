a69b81eb29d5023950384044ea7fa858838a27d0
Sumeyye Karakus <sumeyyekarakusk@gmail.com>
2024-12-04 15:05:16 +0300

    Added functionality to read JSON file

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from utils.data_preprocessing.EmojiRemover import EmojiRemover
+import pandas as pd
+import json
 
 def main():
-    # EmojiRemover class'ını kullan
-    emoji_remover = EmojiRemover()
-    emoji_remover.clean_text()
+    # JSON verisini satır satır okuma
+  with open('C:\\Users\\Huawei\\OneDrive\\Masaüstü\\Haber Sınıflandırma\\NewsClassification\\data\\News_Category_Dataset_v3.json', 'r', encoding='utf-8') as f:
+    data = [json.loads(line) for line in f]
+
+    # DataFrame'e dönüştürme
+    df = pd.DataFrame(data)
+    print(df.head())  # İlk 5 satırı yazdırarak doğru şekilde okunduğunu kontrol edin
 
 if __name__ == "__main__":
     main()
