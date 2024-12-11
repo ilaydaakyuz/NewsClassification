65893748634bedbf03e37aaa78f39946601ebccf
Sumeyye Karakus <sumeyyekarakusk@gmail.com>
2024-12-11 11:13:41 +0300

    The LSTM model has been updated

--- utils/model_training/LSTM.py
+++ utils/model_training/LSTM.py
@@ -1,5 +1,5 @@
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Embedding, LSTM, Dense, Dropout
+from tensorflow.keras.layers import Embedding, LSTM, Dense, Dropout, GlobalMaxPooling1D
 
 class LSTMModel:  # Sınıf adını değiştirin
     def __init__(self, max_words=100000, max_len=100, num_classes=10):
@@ -10,17 +10,17 @@ class LSTMModel:  # Sınıf adını değiştirin
 
     def build_model(self):
         # Doğrudan Keras katmanlarını kullanan bir model oluşturun
-        self.model = Sequential([
+        model = Sequential([
             Embedding(self.max_words, 128, input_length=self.max_len),
-            LSTM(128, return_sequences=True),  # Keras'ın LSTM katmanı
-            LSTM(64),  # İkinci LSTM katmanı
-            Dropout(0.6),
+            LSTM(64, return_sequences=True),  # Keras'ın LSTM katmanı
+            GlobalMaxPooling1D(),
             Dense(64, activation='relu'),
             Dropout(0.6),
             Dense(self.num_classes, activation='softmax')
         ])
-        self.model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-        return self.model
+        model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+        self.model = model
+        return model
 
     def train(self, X_train, y_train, validation_split=0.2, epochs=5, batch_size=32):
         return self.model.fit(X_train, y_train, validation_split=validation_split, epochs=epochs, batch_size=batch_size)
